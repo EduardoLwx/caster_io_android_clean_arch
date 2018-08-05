3bf201657392f00ee70b1ec19541188b2219fa3b
Eduardo Santos <eduardo.lwx@gmail.com>
2018-08-05 16:49:36 -0300

    Setting up the Cache layer data models

---
+++ cache/src/main/java/com/example/cache/db/ProjectConstants.kt
@@ -0,0 +1,10 @@
+package com.example.cache.db
+
+object ProjectConstants {
+
+    const val TABLE_NAME = "projects"
+
+    const val COLUMN_PROJECT_ID = "project_id"
+
+    const val COLUMN_IS_BOOKMARKED = "is_bookmarked"
+}

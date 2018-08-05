b50f51329def657e1fe612ee3a9fac57afd01fa0
Eduardo Santos <eduardo.lwx@gmail.com>
2018-08-05 17:42:35 -0300

    Creating the Configuration Data Access Object

--- cache/src/main/java/com/example/cache/db/ProjectsDatabase.kt
+++ cache/src/main/java/com/example/cache/db/ProjectsDatabase.kt
@@ -5,13 +5,17 @@ import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
 import com.example.cache.dao.CachedProjectsDao
+import com.example.cache.dao.ConfigDao
 import com.example.cache.model.CachedProject
+import com.example.cache.model.Config
 import javax.inject.Inject
 
-@Database(entities = arrayOf(CachedProject::class), version = 1)
+@Database(entities = arrayOf(CachedProject::class, Config::class), version = 1)
 abstract class ProjectsDatabase @Inject constructor(): RoomDatabase() {
 
     abstract fun cachedProjectsDao(): CachedProjectsDao
+
+    abstract fun configDao(): ConfigDao
 
     companion object {
         private var INSTANCE: ProjectsDatabase? = null
